13d3d2138517527d1cf24d48eb036e7a3e53a7ce
sarpsahinalp <sarpsahinalp.official@gmail.com>
2024-12-10 11:06:48 +0100

    Update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/',
+  base: '/SoftwareEngineering-WS2025-just-donate/',
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
   test: {
