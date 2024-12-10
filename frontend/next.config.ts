7a23296375bbd218230c569a52c26cd1ecba07f8
sarpsahinalp <sarpsahinalp.official@gmail.com>
2024-12-10 14:32:26 +0100

    Update config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: '/SoftwareEngineering-WS2025-just-donate',
+    output: "export",
 };
 
 export default nextConfig;
