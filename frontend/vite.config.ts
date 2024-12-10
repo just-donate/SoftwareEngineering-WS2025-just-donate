5bf567221de36a83a1fe75ec6154c1ff5476f477
sarpsahinalp <sarpsahinalp.official@gmail.com>
2024-12-10 11:04:35 +0100

    Update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/',
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
   test: {
