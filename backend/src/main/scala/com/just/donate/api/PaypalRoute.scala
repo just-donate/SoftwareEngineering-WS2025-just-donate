d324920d56e614856ee58156d9f55f072e2cc014
sarpsahinalp <sarpsahinalp.official@gmail.com>
2024-12-25 17:16:46 +0100

    Fix paypal route

--- backend/src/main/scala/com/just/donate/api/PaypalRoute.scala
+++ backend/src/main/scala/com/just/donate/api/PaypalRoute.scala
@@ -6,11 +6,9 @@ import org.http4s.{ HttpRoutes, UrlForm }
 
 object PaypalRoute:
 
-  private val ipnInfo: String = "IPN Endpoint"
-
   val paypalRoute: HttpRoutes[IO] = HttpRoutes.of[IO]:
     case GET -> Root =>
-      Ok(ipnInfo)
+      Ok("IPN Endpoint")
 
     case req @ POST -> Root =>
       for
@@ -18,6 +16,5 @@ object PaypalRoute:
         body <- req.as[UrlForm].map(_.values.toMap)
         _ <- IO.println(s"Received IPN Payload: $body") // Log for debugging
 
-        PaypalRoute.ipnInfo = body.toString()
         response <- Ok(body.toString())
       yield response
