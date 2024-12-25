3a7b3baa69680fe2b4fdf861bb269eb3876488d6
sarpsahinalp <sarpsahinalp.official@gmail.com>
2024-12-25 17:36:20 +0100

    Add temporary list to store IPNs

--- backend/src/main/scala/com/just/donate/api/PaypalRoute.scala
+++ backend/src/main/scala/com/just/donate/api/PaypalRoute.scala
@@ -2,13 +2,17 @@ package com.just.donate.api
 
 import cats.effect.IO
 import org.http4s.dsl.io.*
-import org.http4s.{ HttpRoutes, UrlForm }
+import org.http4s.{HttpRoutes, UrlForm}
+
+import scala.collection.mutable.ListBuffer
 
 object PaypalRoute:
 
+  private val ipnPayloads: ListBuffer[String] = ListBuffer.empty
+
   val paypalRoute: HttpRoutes[IO] = HttpRoutes.of[IO]:
     case GET -> Root =>
-      Ok("IPN Endpoint")
+        Ok(ipnPayloads.mkString("\n"))
 
     case req @ POST -> Root =>
       for
@@ -16,5 +20,6 @@ object PaypalRoute:
         body <- req.as[UrlForm].map(_.values.toMap)
         _ <- IO.println(s"Received IPN Payload: $body") // Log for debugging
 
-        response <- Ok(body.toString())
+        _ <- IO(ipnPayloads += body.toString())
+        response <- Ok("IPN Payload received")
       yield response
