78c9b7a0dbbc256c4eb17e56c5f339eb82b03dc6
lukas-brn <lukas.brennauer@gmail.com>
2025-01-05 20:29:15 +0100

    feat(backend): improve error handling

--- backend/src/main/scala/com/just/donate/api/DonationRoute.scala
+++ backend/src/main/scala/com/just/donate/api/DonationRoute.scala
@@ -2,10 +2,10 @@ package com.just.donate.api
 
 import cats.effect.*
 import com.just.donate.config.Config
-import com.just.donate.models.{ Donation, DonationError, Donor, Organisation }
+import com.just.donate.models.{Donation, DonationError, Donor, Organisation}
 import com.just.donate.notify.IEmailService
 import com.just.donate.store.Store
-import com.just.donate.utils.RouteUtils.loadAndSaveOrganisation
+import com.just.donate.utils.RouteUtils.loadAndSaveOrganisationOps
 import io.circe.*
 import io.circe.generic.auto.*
 import org.http4s.*
@@ -15,41 +15,30 @@ import org.http4s.dsl.io.*
 import cats.implicits.*
 
 object DonationRoute:
+  private val emailTemplate: (String, String, String) => String = (linkWithId, id, link) =>
+    f"""Thank you for your donation, to track your progress visit
+       |${linkWithId}
+       |or enter your tracking id
+       |${id}
+       |on our tracking page
+       |${link}""".stripMargin
+
   val donationRoute: (Store, Config, IEmailService) => HttpRoutes[IO] = (store, config, emailService) =>
     HttpRoutes.of[IO]:
       case req @ POST -> Root / organisationId / "account" / accountName / "donate" =>
-        var trackingId = ""
-        var trackingLink = config.frontendUrl
         for
           requestDonation <- req.as[RequestDonation]
-          response <- loadAndSaveOrganisation(organisationId)(store)(org =>
-            val existingDonor = org.getExistingDonor(requestDonation.donorEmail)
-            val donor = existingDonor.getOrElse(
-              Donor(org.getNewDonorId, requestDonation.donorName, requestDonation.donorEmail)
-            )
-            val donationPart = requestDonation.earmarking match
-              case Some(earmarking) => Donation(donor.id, requestDonation.amount, earmarking)
-              case None             => Donation(donor.id, requestDonation.amount)
-
-            val newOrg = org.donate(donor, donationPart, accountName) match
-              case Left(DonationError.INVALID_ACCOUNT)    => throw new RuntimeException("invalid account")
-              case Left(DonationError.INVALID_EARMARKING) => throw new RuntimeException("invalid earmarking")
-              case Right(value)                           => value
-
-            trackingId = donationPart.donation.donorId
-            trackingLink = f"${trackingLink}/tracking?id=${trackingId}"
-
-            newOrg
-          )
-          _ <- emailService.sendEmail(
-            requestDonation.donorEmail,
-            f"""Thank you for your donation, to track your progress visit
-               |${trackingLink}
-               |or enter your tracking id
-               |${trackingId}
-               |on our tracking page
-               |${config.frontendUrl}""".stripMargin
-          )
+          trackingId <- loadAndSaveOrganisationOps(organisationId)(store)(organisationMapper(requestDonation, accountName))
+          response <- trackingId match
+            case None => BadRequest("Organisation not found")
+            case Some(Left(DonationError.INVALID_ACCOUNT)) => BadRequest("Account not found")
+            case Some(Left(DonationError.INVALID_EARMARKING)) => BadRequest("Earmarking not found")
+            case Some(Right(trackingId)) =>
+              val trackingLink = f"${config.frontendUrl}/tracking?id=${trackingId}"
+              emailService.sendEmail(
+                requestDonation.donorEmail,
+                emailTemplate(trackingLink, trackingId, config.frontendUrl)
+              ) >> Ok()
         yield response
 
   private[api] case class RequestDonation(
@@ -58,3 +47,16 @@ object DonationRoute:
     amount: BigDecimal,
     earmarking: Option[String]
   )
+
+  private def organisationMapper(requestDonation: RequestDonation, accountName: String)(org: Organisation): (Organisation, Either[DonationError, String]) =
+    val existingDonor = org.getExistingDonor(requestDonation.donorEmail)
+    val donor = existingDonor.getOrElse(
+      Donor(org.getNewDonorId, requestDonation.donorName, requestDonation.donorEmail)
+    )
+    val donationPart = requestDonation.earmarking match
+      case Some(earmarking) => Donation(donor.id, requestDonation.amount, earmarking)
+      case None => Donation(donor.id, requestDonation.amount)
+
+    org.donate(donor, donationPart, accountName) match
+      case Left(error) => (org, Left(error))
+      case Right(newOrg) => (newOrg, Right(donationPart.donation.donorId))
