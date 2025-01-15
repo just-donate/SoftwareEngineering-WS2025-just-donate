c87aaa8b05bfe5e27277025aa004232254fe8a1c
AntonKluge <anton.kluge@mailbox.org>
2025-01-15 11:00:22 +0100

    fix backend tests

--- backend/src/main/scala/com/just/donate/api/DonationRoute.scala
+++ backend/src/main/scala/com/just/donate/api/DonationRoute.scala
@@ -23,14 +23,14 @@ object DonationRoute:
     (repository, config, emailService) =>
       HttpRoutes.of[IO]:
 
-        case req@POST -> Root / organisationId / "account" / accountName =>
+        case req @ POST -> Root / organisationId / "account" / accountName =>
           (for
             requestDonation <- req.as[RequestDonation]
             trackingId <- loadAndSaveOrganisationOps(organisationId)(repository)(
               organisationMapper(requestDonation, accountName)
             )
             response <- trackingId match
-              case None => BadRequest("Organisation not found")
+              case None                      => BadRequest("Organisation not found")
               case Some(Left(donationError)) => BadRequest(donationError.message)
               case Some(Right(trackingId)) =>
                 val trackingLink = f"${config.frontendUrl}/tracking?id=$trackingId"
@@ -76,40 +76,45 @@ object DonationRoute:
         Donor(org.getNewDonorId, requestDonation.donorName, requestDonation.donorEmail)
       )
 
-    val earmarking = requestDonation.earmarking.flatMap(org.getEarmarking)
+    val earmarking = requestDonation.earmarking match
+      case Some(value) =>
+        org.getEarmarking(value) match
+          case Some(value) => Some(value)
+          case None        => return (org, Left(DonationError.INVALID_EARMARKING))
+      case None => None
 
     val (donation, donationPart) = earmarking match
       case Some(earmarking) => Donation(donor.id, requestDonation.amount, earmarking)
-      case None => Donation(donor.id, requestDonation.amount)
+      case None             => Donation(donor.id, requestDonation.amount)
 
     org.donate(donor, donationPart, donation, accountName) match
-      case Left(error) => (org, Left(error))
+      case Left(error)   => (org, Left(error))
       case Right(newOrg) => (newOrg, Right(donor.id))
 
   private[api] case class RequestDonation(
-                                           donorName: String,
-                                           donorEmail: String,
-                                           amount: Money,
-                                           earmarking: Option[String]
-                                         )
+    donorName: String,
+    donorEmail: String,
+    amount: Money,
+    earmarking: Option[String]
+  )
 
   // Define the Status case class to represent each status update
   private[api] case class StatusResponse(
-                                          status: String,
-                                          date: LocalDateTime,
-                                          description: String
-                                        )
+    status: String,
+    date: LocalDateTime,
+    description: String
+  )
 
   // Define the Donation case class to represent each donation
   private[api] case class DonationResponse(
-                                            donationId: String,
-                                            amount: Money,
-                                            organisation: String,
-                                            date: LocalDateTime,
-                                            earmarking: Option[String],
-                                            status: Seq[StatusResponse]
-                                          )
+    donationId: String,
+    amount: Money,
+    organisation: String,
+    date: LocalDateTime,
+    earmarking: Option[String],
+    status: Seq[StatusResponse]
+  )
 
   private[api] case class DonationListResponse(
-                                                donations: Seq[DonationResponse]
-                                              )
+    donations: Seq[DonationResponse]
+  )
